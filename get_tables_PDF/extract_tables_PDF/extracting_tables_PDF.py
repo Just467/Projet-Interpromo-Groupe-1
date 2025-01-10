f3feb6d67d3fa3b8d0ff72461df0b32c97a63593
Justin Massabie <justin.massabie@gmail.com>
2025-01-10 14:52:14 +0100

    Create function to get all tables from a multiple files.

--- get_tables_PDF/extract_tables_PDF/extracting_tables_PDF.py
+++ get_tables_PDF/extract_tables_PDF/extracting_tables_PDF.py
@@ -1,25 +1,24 @@
 import pdfplumber
 import sys
 sys.path.append('get_tables_PDF')
-from extract_tables_PDF.functions_extract_tables_PDF import complete_extract_tables_PDF
+from extract_tables_PDF.functions_extract_tables_PDF import get_all_raw_tables_PDF
 from utils.dataframe_viewer import show_dataframes
 
-paths = [
-    r'data\bilans_sociaux\CNP-Assurances-Bilan-social-2023.pdf',
-    r'data\bilans_sociaux\ENGIE SA_Bilan social 2021_VD.pdf',
-    r'data\bilans_sociaux\INSA_bilan_soc_20_V2_21.pdf'
-]
+bilan_sociaux_paths = {'CNP':{'path':r'data\bilans_sociaux\CNP-Assurances-Bilan-social-2023.pdf',
+                              'extract_settings': {},
+                              'methods':['lines', 'explicit']},
+                       'ENGIE':{'path':r'data\bilans_sociaux\ENGIE SA_Bilan social 2021_VD.pdf',
+                              'extract_settings': {},
+                              'methods':['lines', 'lines']},
+                       'INSA':{'path':r'data\bilans_sociaux\INSA_bilan_soc_20_V2_21.pdf',
+                              'extract_settings': {},
+                              'methods':['lines', 'lines']}
+                       }
 
-page_number = 10
-settings = {
-}
+test = {'CNP':{'path':r'data\bilans_sociaux\CNP-Assurances-Bilan-social-2023.pdf',
+               'extract_settings': {},
+               'methods':['lines', 'explicit']}}
 
-with pdfplumber.open(path3) as pdf:
-    page = pdf.pages[page_number]
-    result = complete_extract_tables_PDF(path3,
-                                         page, page_number, settings,
-                                         methods=['lines', 'lines'],
-                                         show_debugging=True)
-    print(result)
-    show_dataframes(result)
+result = get_all_raw_tables_PDF(test, pages = [i for i in range(19)])
+show_dataframes(result['CNP_title'])        
     
--- get_tables_PDF/extract_tables_PDF/functions_extract_tables_PDF.py
+++ get_tables_PDF/extract_tables_PDF/functions_extract_tables_PDF.py
@@ -3,12 +3,14 @@ import pandas as pd
 import numpy as np
 import pdfplumber
 
+
 extract_tables_PDF_methods = ['lines', 'lines_strict', 'explicit']
+
 
 def complete_extract_tables_PDF(pdf_path:str,
                                 page:pdfplumber.page.Page, page_number:int, settings:dict,
                                 methods:list,
-                                show_debugging:bool=False):
+                                show_debugging:bool=False)->list:
     """Extract tables from a page of a PDF using a Page object from pdfplumber
 
     Args:
@@ -31,7 +33,7 @@ def complete_extract_tables_PDF(pdf_path:str,
     if methods[0] not in extract_tables_PDF_methods or methods[1] not in extract_tables_PDF_methods:
         raise ValueError(f"Both values of methods must be one of {", ".join([str(elem) for elem in extract_tables_PDF_methods])}")
     
-    def get_lines_stream(tables:camelot.core.TableList, axis:int):
+    def get_lines_stream(tables:camelot.core.TableList, axis:int)->list:
         """Get the coordinates of lines (of an axis) used by camelot to extract a table with Stream
 
         Args:
@@ -61,11 +63,45 @@ def complete_extract_tables_PDF(pdf_path:str,
         for text_axis in text_axes:
             lines[text_axis] = get_lines_stream(tables, text_axis)
     
-    settings["horizontal_strategy"], settings["vertical_strategy"] = methods[0], methods[1]
-    settings["explicit_horizontal_lines"], settings["explicit_vertical_lines"] = lines[0], lines[1]
-    
-    all_tables = page.extract_tables(settings)
-    df_tables = [pd.DataFrame(table) for table in all_tables]
-    if show_debugging:
-        page.to_image().debug_tablefinder(settings).show()
-    return df_tables
+    if len(lines) > 1:
+        settings["horizontal_strategy"], settings["vertical_strategy"] = methods[0], methods[1]
+        settings["explicit_horizontal_lines"], settings["explicit_vertical_lines"] = lines[0], lines[1]
+        
+        all_tables = page.extract_tables(settings)
+        df_tables = [pd.DataFrame(table) for table in all_tables]
+        if show_debugging:
+            page.to_image().debug_tablefinder(settings).show()
+    else:
+        df_tables = []
+    return 'title', df_tables
+
+
+def get_all_raw_tables_PDF(PDF_file_settings:dict,
+                           dict_tables:dict={}, pages=[-1])->dict:
+    """Function that uses complete_extract_tables_PDF to extract all the tables of multiple PDF files.
+
+    Args:
+        PDF_file_settings (dict): the settings to use for each PDF file.
+        dict_tables (dict, optional): the dict returned by the fuction. Contains all the tables. Defaults to {}.
+        pages (list, optional): list of pages to explore. Defaults to [-1], exploring all pages.
+
+    Returns:
+        dict: returns dict_tables with all the new tables extracted.
+        Tables are organized by PDF_file and by title based on the titles found in the PDF.
+    """
+    for PDF_file_name, settings in PDF_file_settings.items():
+        path, extract_settings, methods = settings['path'], settings['extract_settings'], settings['methods']
+        with pdfplumber.open(path) as pdf:
+            for page_number, page in enumerate(pdf.pages):
+                if page_number in pages or pages==[-1]:
+                    title, tables = complete_extract_tables_PDF(path,
+                                                                page, page_number, extract_settings,
+                                                                methods,
+                                                                show_debugging=False)
+                    full_title = "_".join((PDF_file_name, title))
+                    try:
+                        dict_tables[full_title] = dict_tables[full_title] + tables
+                    except:
+                        dict_tables[full_title] = tables
+               
+    return dict_tables
