57f30dc6b60ae02d1f0db1c5c9eade9d59b1924d
Nicolas Pham <nicolas.pham.hoang@gmail.com>
2025-01-15 13:43:28 +0100

    Starting detect years in corner for year column

--- get_tables_PDF/process_table.py
+++ get_tables_PDF/process_table.py
@@ -31,73 +31,7 @@ preprocess(df, missing_label)
 process_tables(raw_df_list, missing_label="unknown")
 """
 
-
-def format_df(df):
-    """
-    Formate la DataFrame en supprimant les lignes et colonnes vides, 
-    convertissant toutes les valeurs et colonnes en chaînes de caractères
-    
-    args: 
-        df (DataFrame): df à formater
-
-    return:
-        DataFrame: df formatée
-    """
-
-    df = df.dropna(how='all', axis=0)
-    df = df.dropna(how='all', axis=1)
-    df = df.map(lambda x: str(x) if pd.notnull(x) else "")
-    return df
-
-
-def has_numerical_values(series):
-    """
-    Renvoie True si la ligne possède une ou plusieurs valeurs dites numériques
-
-    args:
-        series (pd.Series)
-    return:
-        Boolean
-    """
-    # Contient des chiffres, points, virgules, devises
-    pattern = r"^[\d.,%€$£\s]*$"
-
-    for value in series:
-        if bool(re.match(pattern, value)):
-            return True
-        
-    return False
-
-
-def has_year_sequency(series, series_index, min_year=2020, max_year=2025):
-    """
-    Renvoie True si la pd.Series contient uniquement des années compris dans la plage
-    Avec des règles supplémentaires si c'est la première ligne de la DataFrame
-    
-    args:
-        series (pd.Series): une ligne de DataFrame
-        series_index (int): index de la series (ligne/colonne) dans la DataFrame
-        min_year (int): année minimale autorisée
-        max_year (int): année maximale autorisée
-    return:
-        bool: True si toutes les valeurs numériques sont dans la plage (ou si une seule année avec row_index=0), sinon False.
-    """
-    # Vérifie si la valeur est un nombre entier
-    def is_pure_number(value):
-        return re.match(r"^\d+$", str(value).strip()) is not None
-
-    # Liste des valeurs entières
-    int_values = [int(value) for value in series if is_pure_number(value)]
-
-    # Cas où il y a une seule année (peut-être une année ou un nombre par malchance)
-    if len(int_values) == 1:
-        return series_index == 0 and min_year <= int_values[0] <= max_year
-
-    # Vérifie si toutes les valeurs sont dans la plage d'année
-    return all(min_year <= num <= max_year for num in int_values)
-
-
-def detect_structure(df):
+def detect_structure(df, header_list):
     """
     Renvoie la structure de la df en précisant:
     où se trouvent les headers (en-têtes) et les variables via leur index
@@ -108,21 +42,26 @@ def detect_structure(df):
         dict
     """
 
-    # Pour chaque row, regarde si c'est un header
     header_indexes = []
-    for row_index, row in df.iterrows():
-        isValue = has_numerical_values(row) and not(has_year_sequency(row, row_index))
-        if not isValue:
+    for row_index, row in enumerate(header_list):
+        if row[0] == 'header':
             header_indexes.append(row_index)
-    
+
     # Pour chaque colonne, regarde si c'est une colonne
+    numeric_pattern=r'^[\d.,\s€$£¥]*(?: *ans)?$'
+    year_pattern=r'^20[0-2]\d$'
     variable_indexes = []
     for col_index in range(df.shape[1]):
         column = df.iloc[:, col_index]
-        isValue = has_numerical_values(column) and not(has_year_sequency(column, col_index))
-        if not isValue:
+        isVariable = True
+        for cell in column:
+            if re.match(numeric_pattern, cell) and not re.match(year_pattern, cell):
+                    isVariable = False
+                    break
+            
+        if isVariable:
             variable_indexes.append(col_index)
-    
+
     structure = {
         "headers": header_indexes,
         "variables": variable_indexes
@@ -131,7 +70,7 @@ def detect_structure(df):
     return structure
 
 
-def add_header(df, missing_label):
+def add_header(df, header_list, missing_label):
     """
     Ajoute une ligne tout en haut du DataFrame avec le label spécifié
     
@@ -142,7 +81,7 @@ def add_header(df, missing_label):
         pd.DataFrame: Le DataFrame modifié.
     """
 
-    structure = detect_structure(df)
+    structure = detect_structure(df, header_list)
     nb_headers = len(structure["headers"])
 
     if nb_headers > 0:
@@ -151,14 +90,12 @@ def add_header(df, missing_label):
     # Créer une nouvelle ligne avec le label
     new_row = pd.DataFrame([[missing_label] * df.shape[1]], columns=df.columns)
     df = pd.concat([new_row, df], ignore_index=True)
-    
-    # Réindexation
     df.columns = range(df.shape[1])
     
     return df
 
 
-def remove_totals(df):
+def remove_totals(df, header_list):
     """
     Enlève les lignes et colonnes de type "Total" de la df
 
@@ -168,7 +105,7 @@ def remove_totals(df):
     return:
         dict
     """
-    structure = detect_structure(df)
+    structure = detect_structure(df, header_list)
     
     all_total_col_indexes = []
     for header_index in structure["headers"]:
@@ -199,8 +136,8 @@ def remove_totals(df):
     return df
 
 
-def fill_variable_names(df, missing_label):
-    structure = detect_structure(df)
+def fill_variable_names(df, header_list, missing_label):
+    structure = detect_structure(df, header_list)
 
     nb_headers = len(structure["headers"])
     variable_indexes = structure["variables"]
@@ -212,20 +149,16 @@ def fill_variable_names(df, missing_label):
 
         # Vérifiez si col_names est vide
         if len(col_names) == 0:
-            col_name = missing_label  # Si vide, utilisez une valeur par défaut
-        else:
-            col_name = col_names[-1]
+            df.iloc[:nb_headers, col_index] = missing_label
 
         if previous_col_names is not None and np.array_equal(col_names, previous_col_names):
-            col_name = missing_label  # Si les noms sont identiques, utilisez missing_label
+            df.iloc[:nb_headers, col_index] = missing_label
         
-        df.iloc[:nb_headers, col_index] = col_name
         previous_col_names = col_names
-    
     return df
 
 
-def fill_headers(df, missing_label):
+def fill_headers(df, header_list, missing_label):
     """
     Remplit les valeurs manquantes des lignes spécifiées par `header_indexes`
     et remplace les valeurs restantes par "unknown".
@@ -236,8 +169,9 @@ def fill_headers(df, missing_label):
     Returns:
         pd.DataFrame: Le DataFrame avec les headers remplis.
     """
+
     # Détection de la structure pour obtenir les lignes des headers
-    structure = detect_structure(df)
+    structure = detect_structure(df, header_list)
     header_indexes = structure["headers"]
 
     df.iloc[header_indexes, :] = df.iloc[header_indexes, :].replace("", None)
@@ -251,7 +185,7 @@ def fill_headers(df, missing_label):
     return df
 
 
-def fill_variables(df):
+def fill_variables(df, header_list):
     """
     Remplit les colonnes de variables vers le bas (forward fill).
     Si une seule colonne est présente, elle est également remplie.
@@ -262,7 +196,7 @@ def fill_variables(df):
     Returns:
         pd.DataFrame: Le DataFrame avec les variables remplies.
     """
-    structure = detect_structure(df)
+    structure = detect_structure(df, header_list)
     variable_indexes = structure["variables"]
 
     # Vérifier s'il y a des variables à remplir
@@ -331,7 +265,7 @@ def format_colnames(df, missing_label="unknown"):
     simplified = []
     for colname in df.columns:
         # Détermine le nom de base (premier élément pour MultiIndex)
-        primary_name = colname[0] if isinstance(colname, tuple) else colname
+        primary_name = colname[-1] if isinstance(colname, tuple) else colname
 
         if missing_label in primary_name:
             unique_values = df[colname].dropna().unique()
@@ -377,81 +311,7 @@ def add_unit_column(df, value_colname="value", unit_colname="unit", default_unit
     return df
 
 
-def split_df(df):
-    """
-    Divise la DataFrame en plusieurs sous-DataFrames en fonction des headers et des variables trouvées
-    
-    args:
-        df (pd.DataFrame): le DataFrame d'origine
-        structure (dict):
-            "headers": liste des index des headers
-            "variables": liste des index des variables
-    
-    Returns:
-        list: liste des sous-DataFrames découpés
-    """
-    structure = detect_structure(df)
-    splitted_dfs = []
-    
-    # Obtenir les index des headers (lignes) et des variables (colonnes)
-    header_indexes = structure["headers"]
-    variable_indexes = structure["variables"]
-
-    # Identifier les intervalles pour les headers
-    header_intervals = []
-    if header_indexes:  # Vérifier qu'il y a au moins un header
-        current_start = header_indexes[0]  # Initialiser au premier header
-        for i in range(len(header_indexes) - 1):
-            if header_indexes[i + 1] > header_indexes[i] + 1:  # Gap détecté
-                header_intervals.append((current_start, header_indexes[i] + 1))
-                current_start = header_indexes[i + 1]  # Début du prochain intervalle
-        # Ajouter le dernier intervalle
-        header_intervals.append((current_start, None))
-
-    # Identifier les intervalles pour les variables
-    variable_intervals = []
-    if variable_indexes:  # Vérifier qu'il y a au moins une variable
-        current_start = variable_indexes[0]  # Initialiser à la première variable
-        for i in range(len(variable_indexes) - 1):
-            if variable_indexes[i + 1] > variable_indexes[i] + 1:  # Gap détecté
-                variable_intervals.append((current_start, variable_indexes[i] + 1))
-                current_start = variable_indexes[i + 1]  # Début du prochain intervalle
-        # Ajouter le dernier intervalle
-        variable_intervals.append((current_start, None))
-
-    # Découper la DataFrame en fonction des intervalles
-    for h_start, h_end in header_intervals:
-        for v_start, v_end in variable_intervals:
-            # Si h_end ou v_end est None, cela signifie "jusqu'à la fin"
-            sub_df = df.iloc[
-                h_start:(h_end if h_end is not None else None),  # Lignes
-                v_start:(v_end if v_end is not None else None)   # Colonnes
-            ]
-            # Réindexer le sous-DataFrame
-            sub_df = sub_df.reset_index(drop=True)  # Réinitialiser l'index des lignes
-            sub_df.columns = range(sub_df.shape[1])  # Réinitialiser les colonnes
-            splitted_dfs.append(sub_df)
-
-    return splitted_dfs
-
-
-def clean_df(df, missing_label="unknown"):
-    """
-    Applique un nettoyage puis détecte s'il y a plusieurs dfs et les split
-    permettant de dépivoter la df correctement
-    """
-    df = format_df(df)
-    df = fill_headers(df, missing_label)
-    df = fill_variables(df)
-    df = remove_totals(df)
-
-    df_splitted = split_df(df)
-    df_splitted = [fill_variable_names(df, missing_label) for df in df_splitted]
-
-    return df_splitted
-
-
-def unpivot_df(df, value_colname="value", unit_colname="unit", default_unit="nombre", missing_label="unknown"):
+def unpivot_df(df, header_list, current_year="2023", year_colname="Année", value_colname="Valeur", unit_colname="Unité", default_unit="nombre", missing_label="unknown"):
     """
     Dépivote les colonnes d'une df cleaned
     
@@ -462,14 +322,21 @@ def unpivot_df(df, value_colname="value", unit_colname="unit", default_unit="nom
 
     return: DataFrame dépivotée
     """
-    structure = detect_structure(df)
+    structure = detect_structure(df, header_list)
     nb_headers = len(structure["headers"])
     nb_variables = len(structure["variables"])
-  
     headers = df.iloc[:nb_headers]
 
+    year_pattern = r'^20[0-2]\d$'
+    has_year_corner = False
+    if nb_headers > 0:
+        top_left_corner = df.iloc[nb_headers-1, 0]
+        if re.match(year_pattern, top_left_corner):
+            df.iloc[:nb_headers, 0] = ""
+            has_year_corner = True
+
     # Crée le MultiIndex (en-têtes)
-    df = add_header(df, missing_label)
+    df = add_header(df, header_list, missing_label)
     df.columns = pd.MultiIndex.from_frame(headers.T)
     df.columns = clean_multi_index(df.columns)
 
@@ -487,23 +354,14 @@ def unpivot_df(df, value_colname="value", unit_colname="unit", default_unit="nom
 
     # Ajout de la colonne "unit"
     df_unpivot = add_unit_column(df_unpivot, value_colname, unit_colname, default_unit)
+
+    # Ajout de la colonne "Année"
+    if has_year_corner:
+        df_unpivot[year_colname] = top_left_corner
+    else:
+        df_unpivot[year_colname] = current_year
+
     return df_unpivot
-
-# data1 = [
-#     [None, None, "Homme", None, "Femme", None],
-#     [None, "categ", "Fonctionnaire", "Non Fonctionnaire", "Fonctionnaire", "Non Fonctionnaire"],
-#     [2021, "Catégorie A", "8 €", 9, 10, 11],
-#     [2021, "Catégorie B", 15, 25, 20, 30],
-#     [2021, "Catégorie C", 45, 35, 40, 25],
-#     [2022, "Catégorie A", 50, 55, 60, 65],
-#     [2022, "Catégorie B", 70, 75, 80, 85]
-# ]
-
-# df = pd.DataFrame(data1)
-
-# cleaned_df = clean_df(df)
-# print(cleaned_df)
-# # print(unpivot_df(cleaned_df))
 
 
 def split_dataframe(df:pd.core.frame.DataFrame, indexes:list)->list:
@@ -523,7 +381,7 @@ def split_dataframe(df:pd.core.frame.DataFrame, indexes:list)->list:
                 if index != indexes[index_pos-1][0] + 1:
                         df_list.append( (df.iloc[previous_index:index], top) )
                         previous_index = index
-        df_list.append((df.iloc[index:], top))
+        df_list.append((df.iloc[previous_index:], top))
         return df_list
     else:
         return [(df, 0)]
@@ -535,11 +393,20 @@ def clean_df(df, header_list,
     Applique un nettoyage puis détecte s'il y a plusieurs dfs et les split
     permettant de dépivoter la df correctement
     """
-    df = format_df(df)
-    df = fill_headers(df, missing_label)
-    df = fill_variables(df)
+    # Splitting
     index_none_row = [i for i, (header, top) in enumerate(header_list) if header=='none']
     header_list = [(header, top) for header, top in header_list if header!='none']
     df = df.drop(index_none_row)
     df_list = split_dataframe(df, [(i, top) for i, (header, top) in enumerate(header_list) if header])
-    return df_list 
+
+    # Formatting
+    final_df_list = []
+    # for df, top, header_list in df_list:
+    for df, top in df_list:
+        df = df.fillna("").astype(str)
+        df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+        df = fill_headers(df, header_list, missing_label)
+        df = fill_variable_names(df, header_list, missing_label)
+        df = fill_variables(df, header_list)
+        final_df_list.append(df)
+    return final_df_list 
