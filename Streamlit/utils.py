10ea3b7073a2fa4b0cc5948a9ef3f716e7334a36
EmelineKleinhans <emelinekleinhans@gmail.com>
2025-01-15 11:22:03 +0100

    Amélioration des fonctions dans utils.py

--- Streamlit/utils.py
+++ Streamlit/utils.py
@@ -15,15 +15,15 @@ style_container = """{
     background-color: #ffffff;
     }""" 
 
-
 ##############################################################
 #--########-------DEFINITION DES FONCTIONS----######
 ##############################################################
-
-def selection_menu (dossier_entreprise, col_inutiles):
+def importation_data (dossier_entreprise, col_inutiles):
 
     """Fonction qui prends en paramètre le chemin relatif du dossier contenant les données d'une entreprise et les noms de colonnes inutiles
-    et retourne la thématique choisie (selection), l'indicateur choisi (indicateur_), les données en lien (df) et les axes d'analyse (dimension_1 et dimension_2)"""
+    et retourne la sélection de thémathiques et un dictionnaire (resultats) contenant pour la ou les thématiques choisies:
+    - les données associées (entreprise_data) 
+    - et la liste de l'ensemble des indicateurs (indicateurs) """
 
     #----selection Thématique-----------------
     sous_dossiers = [nom for nom in os.listdir(dossier_entreprise) if os.path.isdir(os.path.join(dossier_entreprise, nom))]
@@ -32,7 +32,7 @@ def selection_menu (dossier_entreprise, col_inutiles):
     #----IMPORTATION DE LA DATA----------------
     if not selection:
         st.write("Veuilliez sélectionner au moins une thématique.")
-        return ([],None,[],None,None)
+        return ([],{},[])
     else:
         # initialisation dictionnaire pour répertorier les indicateurs et leurs données associées 
         resultats={}
@@ -57,53 +57,63 @@ def selection_menu (dossier_entreprise, col_inutiles):
             resultats[s] = {'entreprise_data': entreprise_data, 'indicateurs': indicateurs}
         # lister tous les indicateurs du dictionnaire 
         liste_indicateurs = []
-        for dossier, contenu in resultats.items(): 
+        for selection, contenu in resultats.items(): 
             for indicateur in contenu['indicateurs']: 
                 liste_indicateurs.append(indicateur)
+        return (selection, resultats, liste_indicateurs)
+    
+def selection_menu (selection, resultats, liste_indicateurs):
+    
+    """Fonction qui prends en paramètres la sélection de thématiques, le dictionnaire resultats et la liste des indicateurs associés
+    et retourne la thématique choisie (selection), l'indicateur choisi (indicateur_), les données en lien (df) et les axes d'analyse (dimension_1 et dimension_2)"""
+    
+    #----selection d'indicateurs--------
+    if selection:  
+        st.sidebar.subheader("Indicateur")
+        indicateur_ = st.sidebar.selectbox(
+                "Veuillez choisir un indicateur :",
+                liste_indicateurs,
+                index=None,
+                placeholder="Sélectionnez un indicateur...",
+            )
+        # récupération données liées à l'indicateur choisi 
+        for selection, contenu in resultats.items(): 
+            for indicateur in contenu['indicateurs']: 
+                if indicateur == indicateur_:
+                    entreprise_data=contenu['entreprise_data']
+        #df = entreprise_data[entreprise_data["Indicateur"] == indicateur_].dropna(axis=1, how='all')
+                    df = entreprise_data.dropna(axis=1, how='all')
+                    dimension = df.select_dtypes(include=["object", "category"]).columns.tolist()
+                    if indicateur_: 
+                        dimension.remove("Indicateur")
+                        dimension.remove("Unité")
 
-        #----selection d'indicateurs--------
-        if selection:  
-            st.sidebar.subheader("Indicateur")
-            indicateur_ = st.sidebar.selectbox(
-                    "Veuillez choisir un indicateur :",
-                    liste_indicateurs,
-                    index=None,
-                    placeholder="Sélectionnez un indicateur...",
-                )
-            # récupération données liées à l'indicateur choisi 
-            for dossier, contenu in resultats.items(): 
-                for indicateur in contenu['indicateurs']: 
-                    if indicateur == indicateur_:
-                        entreprise_data=contenu['entreprise_data']
-            df = entreprise_data[entreprise_data["Indicateur"] == indicateur_]
-            dimension = df.select_dtypes(include=["object", "category"]).columns.tolist()
-            dimension.remove("Indicateur")
-
-            #----selection axes d'analyse--------
-            if indicateur_:
-                st.sidebar.subheader("Axes d'analyse")
-                dimension_1 = st.sidebar.selectbox("Veuillez choisir le 1er axe d'analyse :",dimension, index=None, placeholder="Sélectionnez un axe d'analyse...") 
-                if dimension_1:
-                    reste = [d for d in dimension if d != dimension_1]
-                    dimension_2 = st.sidebar.selectbox("Veuillez choisir le 2eme axe d'analyse :",reste, index=None, placeholder="Sélectionnez un axe d'analyse...")
-                    return (selection,indicateur_,df,dimension_1,dimension_2)
-                else: 
-                    st.write("Veuillez sélectionner au moins un axe d'analyse.")
-                    return (selection,indicateur_,df,None,None)
-            else:
-                return (selection,None,df,None,None)
+        #----selection axes d'analyse--------
+        if indicateur_:
+            st.sidebar.subheader("Axes d'analyse")
+            dimension_1 = st.sidebar.selectbox("Veuillez choisir le 1er axe d'analyse :",dimension, index=None, placeholder="Sélectionnez un axe d'analyse...") 
+            if dimension_1:
+                reste = [d for d in dimension if d != dimension_1]
+                dimension_2 = st.sidebar.selectbox("Veuillez choisir le 2eme axe d'analyse :",reste, index=None, placeholder="Sélectionnez un axe d'analyse...")
+                return (selection,indicateur_,df,dimension_1,dimension_2)
+            else: 
+                return (selection,indicateur_,df,None,None)
+        else:
+            st.write("Veuillez saisir un indicateur.")
+            return (selection,None,[],None,None)
+        
             
-def titre (texte):
-    """Fonction qui prend en paramètre le texte du titre
+def titre (variable):
+    """Fonction qui prend en paramètre la variable du titre
     et retourne l'affichage."""
     st.markdown(f"""<div style='text-align: center; 
                 font_size: 20px;
-                font-weight: bold;'> 
-                {texte} </div>""",
+                font-weight: bold;'> Evolution de l'indicateur 
+                <i>{variable.lower()}</i> par année </div>""",
                 unsafe_allow_html=True)
     
-def mise_en_forme_graph (nom_fig, key_fig, indicateur_):
-    """Fonction qui prend en paramètre le nom de la figure, son indentfiant et l'indicateur en lien  
+def mise_en_forme_graph (nom_fig, key_fig, indicateur_, df):
+    """Fonction qui prend en paramètre le nom de la figure, son indentfiant, l'indicateur et ses données en lien  
     et retourne la mise en forme permattant l'affichage du graphique."""
     with stylable_container(key=key_fig,
                             css_styles=style_container): 
@@ -111,8 +121,76 @@ def mise_en_forme_graph (nom_fig, key_fig, indicateur_):
                     nom_fig.update_layout(
                         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5),  # Légende en haut
                         margin=dict(l=10, r=10, t=0, b=10),  # Marges pour éviter que ça soit trop collé
-                        height=300  # Taille réduite du graphique pour qu'il rentre bien
+                        height=300,  # Taille réduite du graphique pour qu'il rentre bien
+                        yaxis_title="Valeur en "+df["Unité"].iloc[0]
                     ),
                     use_container_width=True
                 )
-    titre(f"Evolution des {indicateur_} des employés par année")
+    titre(indicateur_)
+
+def affichage_graphs (selection, indicateur_, df, dimension_1, dimension_2):
+    """Fonction qui prends en paramètres la ou les thématiques, l'indicateur choisi, les données en lien et les axes d'analyse choisis
+    et qui retourne l'affichage de l'ensembles des graphiques."""
+
+    #----------------Graph simple évol au cours du temps---------
+    if selection and indicateur_ and not dimension_1:
+        df_grouped = df.groupby(["Année"], as_index=False).sum()
+
+        fig_ligne=px.line(df_grouped, x="Année",
+                        y="Valeur", 
+                        markers=True,
+                        color_discrete_sequence=px.colors.qualitative.D3
+                        )
+        fig_bar=px.bar(df_grouped, x="Année",
+                y="Valeur",
+                color=dimension_1,
+                color_discrete_sequence=px.colors.qualitative.D3 )
+
+        a,b=st.columns(2)
+        with a:
+            mise_en_forme_graph (fig_bar, "graph_containera", indicateur_, df)
+        with b:
+            mise_en_forme_graph (fig_ligne, "graph_containerb", indicateur_, df)
+
+    #----------------Graph univarié-----------------------------
+    elif selection and indicateur_ and dimension_1 and not dimension_2:
+        df_grouped = df.groupby(["Année",dimension_1], as_index=False).sum()
+
+        fig_ligne=px.line(df_grouped, x="Année",
+                        y="Valeur", 
+                        color=dimension_1,
+                        labels={dimension_1, "Valeur: "+indicateur_},
+                        markers=True,
+                        color_discrete_sequence=px.colors.qualitative.D3
+                        )
+        fig_bar=px.bar(df_grouped, x="Année",
+                y="Valeur",
+                color=dimension_1,
+                barmode="group",
+                labels={dimension_1, "Valeur: "+indicateur_},
+                color_discrete_sequence=px.colors.qualitative.D3 )
+
+        a,b=st.columns(2)
+        with a:
+            mise_en_forme_graph (fig_bar, "graph_containera", indicateur_, df)
+        with b:
+            mise_en_forme_graph (fig_ligne, "graph_containerb", indicateur_, df)
+    elif selection and indicateur_ and dimension_1:
+    #----------------Graph multiple----------------------------
+
+        df_grouped = df.groupby(["Année",dimension_1,dimension_2], as_index=False).sum()
+        fig_secteur = px.sunburst(df_grouped, path=["Année",dimension_1,dimension_2], values="Valeur")
+        fig_multi_lignes = px.line(df_grouped, x="Année", y="Valeur", 
+                                    color=dimension_1, line_dash=dimension_2,
+                                    markers=True)
+
+        col1, col2 = st.columns(2)
+
+        # Colonne 1 : Secteur 
+        with col1:
+            mise_en_forme_graph (fig_secteur, "graph_container1", indicateur_, df)
+
+        # Colonne 2 : Multi-lignes
+        with col2:
+            mise_en_forme_graph (fig_multi_lignes, "graph_container2", indicateur_, df)
+    
